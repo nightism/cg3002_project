db16cf7843f4b26b394a31baef068c7add85e121
nightism <sun.mingyang@gmx.com>
2018-09-21 13:26:40 +0800

    improve data reader

--- PredictionComponent2/import_data.py
+++ PredictionComponent2/import_data.py
@@ -1,34 +1,48 @@
 import numpy as np
 import pandas as pd
 
+import preprocess_data as pre
+
 # data from http://archive.ics.uci.edu/ml/datasets/Daily+and+Sports+Activities#
 
 
-def import_data():
+def import_data(label_end=4, object_end=9, seg_end=25, label_start=1, object_start=1, seg_start=1):
 
     x = None
     y = None
 
-    for i in range(1, 20):  # from 01 to 19
+    for i in range(label_start, label_end):  # from 01 to 19, labels
         folder_name = str('{num:02d}'.format(num=i))
-        for j in range(1, 2):  # from 1 to 9
-            subfolder_name = str(j)
-            for k in range(1, 8):  # from 01 to 60
-                file_num = str('{num:02d}'.format(num=k))
-                this_x = pd.read_csv("./data/a" + folder_name + "/p" + subfolder_name + "/s" + file_num + ".txt").values
-                this_y = np.empty(len(this_x))
-                this_y.fill(i)
+        this_label_x = []
 
-                if x is None:
-                    x = this_x
-                    y = this_y
-                else:
-                    x = np.concatenate((x, this_x), axis=0)
-                    y = np.concatenate((y, this_y), axis=0)
+        for j in range(object_start, object_end):  # from 1 to 9, objects
+            subfolder_name = str(j)
+            for k in range(seg_start, seg_end):  # from 01 to 61, segments
+                file_num = str('{num:02d}'.format(num=k))
+                this_x = pd.read_csv("./data/a" + folder_name + "/p" + subfolder_name + "/s" + file_num + ".txt", header = None).values
+                # print(this_x.shape[0])
+                # print(this_x.shape[1])
+                # print(this_x)
+                # this_x = this_x.reshape(1, 125 * 45)
+                this_label_x = np.append(this_label_x, this_x)
+
+        this_label_x = this_label_x.reshape((object_end - object_start) * (seg_end - seg_start) * 125, 45)
+        this_label_x = pre.preprocess_data(this_label_x)
+
+        this_y = np.empty(this_label_x.shape[0])
+        this_y.fill(i)
+
+        if x is None:
+            x = this_label_x
+            y = this_y
+        else:
+            x = np.concatenate((x, this_label_x), axis=0)
+            y = np.concatenate((y, this_y), axis=0)
+
     return x, y
 
 
 if __name__ == '__main__':
-    x, y = import_data()
+    x, y = import_data(label_end=4, object_end=9, seg_end=25)
     print(len(x))
     print(len(y))
